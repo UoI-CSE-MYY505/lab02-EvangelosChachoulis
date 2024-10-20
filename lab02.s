96082ae0327f54437b8c3eb51753dee41a2e8595
EvangelosChachoulis <chachoulise@gmail.com>
2024-10-20 12:30:42 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -8,11 +8,27 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     la a0, array
     li a1, 7    # unsigned
     li a2, 1
+    beq a1, zero, setzero
+    addi t3, a1, -1
+    
 prog:
 #-----------------------------
 # Write your code here!
 # Do not remove the prog label or write code above it!
 #-----------------------------
+    slli t0, t3, 2
+    add t0, t0, a0
+    lw t1, 0(t0)
+    beq t1, a2, set
+    addi t3, t3, -1
+    j prog
+
+setzero:
+    li s0, 0
+    
+set:
+    add s0, t0, zero
+    
 done:
     addi a7, zero, 10 
     ecall
